6f408a64c59fbf7751e963884258c2d4faaf4738
JoaoVictorRodrigues <joaovrs1@al.insper.edu.br>
2020-10-19 20:19:39 -0300

    Conceito C sem o timeout + led acendendo quando clica

--- OLED-Xplained-Pro-SPI/src/main.c
+++ OLED-Xplained-Pro-SPI/src/main.c
@@ -1,12 +1,12 @@
 #include <asf.h>
 
-#include "gfx_mono_ug_2832hsweg04.h"
-#include "gfx_mono_text.h"
-#include "sysfont.h"
+//#include "gfx_mono_ug_2832hsweg04.h"
+//#include "gfx_mono_text.h"
+//#include "sysfont.h"
 
 /**
- * IOS
- */
+* IOS
+*/
 #define BUT1_PIO          PIOD
 #define BUT1_PIO_ID       ID_PIOD
 #define BUT1_PIO_IDX      28
@@ -43,9 +43,10 @@
 
 // sequencia
 enum LED {LED1 = 1, LED2, LED3} LEDS;
-
+// enum MASK_LED {LED1_IDX_MASK = 1, LED2_IDX_MASK,LED3_IDX_MASK} MASKS;
 int seq0[] = {LED1, LED3, LED2};
 int seq0_len = sizeof(seq0)/sizeof(seq0[0]);
+int seq0_mask[] = {LED1_IDX_MASK,LED3_IDX_MASK,LED2_IDX_MASK};
 
 // flags
 volatile char flag_but1 = 0;
@@ -57,7 +58,7 @@ volatile char flag_rtt  = 0;
 void BUT1_callback(void);
 void BUT2_callback(void);
 void BUT3_callback(void);
-void genius_play(int seq[], int seq_len, int delay);
+int genius_play(int seq[], int seq_len, int delay, int seq_mask[]);
 int user_play(int seq[], int seq_len);
 
 /**************************************************************/
@@ -65,30 +66,33 @@ int user_play(int seq[], int seq_len);
 /**************************************************************/
 
 void RTT_Handler (void) {
-  uint32_t ul_status;
+	uint32_t ul_status;
 
-  /* Get RTT status - ACK */
-  ul_status = rtt_get_status(RTT);
+	/* Get RTT status - ACK */
+	ul_status = rtt_get_status(RTT);
 
-  /* IRQ due to Time has changed */
-  if ((ul_status & RTT_SR_RTTINC) == RTT_SR_RTTINC) {  }
+	/* IRQ due to Time has changed */
+	if ((ul_status & RTT_SR_RTTINC) == RTT_SR_RTTINC) {  }
 
-  /* IRQ due to Alarm */
-  if ((ul_status & RTT_SR_ALMS) == RTT_SR_ALMS) {
-      flag_rtt = 1;                
-   }
+	/* IRQ due to Alarm */
+	if ((ul_status & RTT_SR_ALMS) == RTT_SR_ALMS) {
+		flag_rtt = 1;
+	}
 }
 
 void BUT1_callback (void) {
-  flag_but1 = 1;
+	flag_but1 = 1;
+	pio_clear(LED1_PIO, LED1_IDX_MASK);
 }
 
 void BUT2_callback (void) {
-  flag_but2 = 1;
+	flag_but2 = 1;
+	pio_clear(LED2_PIO, LED2_IDX_MASK);
 }
 
 void BUT3_callback (void) {
-  flag_but3 = 1;
+	flag_but3 = 1;
+	pio_clear(LED3_PIO, LED3_IDX_MASK);
 }
 
 /**************************************************************/
@@ -99,16 +103,18 @@ void init (void) {
 	board_init();
 	sysclk_init();
 	delay_init();
-	//SysTick_Config(sysclk_get_cpu_hz() / 1000); // 1 ms
-	//gfx_mono_ssd1306_init();
+	//	SysTick_Config(sysclk_get_cpu_hz() / 1000); // 1 ms
+
+	//	gfx_mono_ssd1306_init();
 
 	pmc_enable_periph_clk(BUT1_PIO_ID);
 	pmc_enable_periph_clk(BUT2_PIO_ID);
 	pmc_enable_periph_clk(BUT3_PIO_ID);
+	
+	pio_set_debounce_filter(BUT1_PIO, BUT1_PIO_IDX_MASK, 100);
+	pio_set_debounce_filter(BUT2_PIO, BUT2_PIO_IDX_MASK, 100);	pio_set_debounce_filter(BUT3_PIO, BUT3_PIO_IDX_MASK, 100);
 
-	pio_set_input(BUT1_PIO,BUT1_PIO_IDX_MASK,PIO_DEFAULT);
-	pio_set_input(BUT2_PIO,BUT2_PIO_IDX_MASK,PIO_DEFAULT);
-	pio_set_input(BUT3_PIO,BUT3_PIO_IDX_MASK,PIO_DEFAULT);
+	pio_set_input(BUT1_PIO,BUT1_PIO_IDX_MASK,PIO_DEFAULT | PIO_DEBOUNCE);	pio_set_input(BUT2_PIO,BUT2_PIO_IDX_MASK,PIO_DEFAULT | PIO_DEBOUNCE);	pio_set_input(BUT3_PIO,BUT3_PIO_IDX_MASK,PIO_DEFAULT | PIO_DEBOUNCE);
 
 	pio_pull_up(BUT1_PIO,BUT1_PIO_IDX_MASK,1);
 	pio_pull_up(BUT2_PIO,BUT2_PIO_IDX_MASK,1);
@@ -118,9 +124,9 @@ void init (void) {
 	pio_enable_interrupt(BUT2_PIO, BUT2_PIO_IDX_MASK);
 	pio_enable_interrupt(BUT3_PIO, BUT3_PIO_IDX_MASK);
 
-  pio_get_interrupt_status(BUT1_PIO);
-  pio_get_interrupt_status(BUT2_PIO);
-  pio_get_interrupt_status(BUT3_PIO);
+	pio_get_interrupt_status(BUT1_PIO);
+	pio_get_interrupt_status(BUT2_PIO);
+	pio_get_interrupt_status(BUT3_PIO);
 
 	NVIC_EnableIRQ(BUT1_PIO_ID);
 	NVIC_SetPriority(BUT1_PIO_ID, 5); // Priority 2
@@ -131,38 +137,38 @@ void init (void) {
 	NVIC_EnableIRQ(BUT3_PIO_ID);
 	NVIC_SetPriority(BUT3_PIO_ID, 5); // Priority 2
 
-	pio_handler_set(BUT1_PIO, BUT1_PIO_ID, BUT1_PIO_IDX_MASK, PIO_IT_EDGE, BUT1_callback);
-	pio_handler_set(BUT2_PIO, BUT2_PIO_ID, BUT2_PIO_IDX_MASK, PIO_IT_EDGE, BUT2_callback);
-	pio_handler_set(BUT3_PIO, BUT3_PIO_ID, BUT3_PIO_IDX_MASK, PIO_IT_EDGE, BUT3_callback);
+	pio_handler_set(BUT1_PIO, BUT1_PIO_ID, BUT1_PIO_IDX_MASK, PIO_IT_FALL_EDGE, BUT1_callback);
+	pio_handler_set(BUT2_PIO, BUT2_PIO_ID, BUT2_PIO_IDX_MASK, PIO_IT_FALL_EDGE, BUT2_callback);
+	pio_handler_set(BUT3_PIO, BUT3_PIO_ID, BUT3_PIO_IDX_MASK, PIO_IT_FALL_EDGE, BUT3_callback);
 
-  pmc_enable_periph_clk(LED1_PIO_ID);
-	pio_set_output(LED1_PIO, LED1_IDX_MASK, estado, 0, 0);
+	pmc_enable_periph_clk(LED1_PIO_ID);
+	pio_set_output(LED1_PIO, LED1_IDX_MASK, 1, 0, 0);
 
-  pmc_enable_periph_clk(LED2_PIO_ID);
-	pio_set_output(LED2_PIO, LED2_IDX_MASK, estado, 0, 0);
+	pmc_enable_periph_clk(LED2_PIO_ID);
+	pio_set_output(LED2_PIO, LED2_IDX_MASK, 1, 0, 0);
 
-  pmc_enable_periph_clk(LED3_PIO_ID);
-	pio_set_output(LED3_PIO, LED3_IDX_MASK, estado, 0, 0);
+	pmc_enable_periph_clk(LED3_PIO_ID);
+	pio_set_output(LED3_PIO, LED3_IDX_MASK, 1, 0, 0);
 }
 
 void RTT_init(uint16_t pllPreScale, uint32_t IrqNPulses) {
-  uint32_t ul_previous_time;
+	uint32_t ul_previous_time;
 
-  /* Configure RTT for a 1 second tick interrupt */
-  rtt_sel_source(RTT, false);
-  rtt_init(RTT, pllPreScale);
+	/* Configure RTT for a 1 second tick interrupt */
+	rtt_sel_source(RTT, false);
+	rtt_init(RTT, pllPreScale);
 
-  ul_previous_time = rtt_read_timer_value(RTT);
-  while (ul_previous_time == rtt_read_timer_value(RTT));
+	ul_previous_time = rtt_read_timer_value(RTT);
+	while (ul_previous_time == rtt_read_timer_value(RTT));
 
-  rtt_write_alarm_time(RTT, IrqNPulses+ul_previous_time);
+	rtt_write_alarm_time(RTT, IrqNPulses+ul_previous_time);
 
-  /* Enable RTT interrupt */
-  NVIC_DisableIRQ(RTT_IRQn);
-  NVIC_ClearPendingIRQ(RTT_IRQn);
-  NVIC_SetPriority(RTT_IRQn, 4);
-  NVIC_EnableIRQ(RTT_IRQn);
-  rtt_enable_interrupt(RTT, RTT_MR_ALMIEN | RTT_MR_RTTINCIEN);
+	/* Enable RTT interrupt */
+	NVIC_DisableIRQ(RTT_IRQn);
+	NVIC_ClearPendingIRQ(RTT_IRQn);
+	NVIC_SetPriority(RTT_IRQn, 4);
+	NVIC_EnableIRQ(RTT_IRQn);
+	rtt_enable_interrupt(RTT, RTT_MR_ALMIEN | RTT_MR_RTTINCIEN);
 }
 
 void play_led1 (int time) {
@@ -175,61 +181,135 @@ void play_led3 (int time) {
 }
 
 void start_rtt (void) {
-  uint16_t pllPreScale = (int) (((float) 32768) / 4.0);
-  uint32_t irqRTTvalue = 12;
+	uint16_t pllPreScale = (int) (((float) 32768) / 4.0);
+	uint32_t irqRTTvalue = 12;
 
-  // reinicia RTT para gerar um novo IRQ
-  RTT_init(pllPreScale, irqRTTvalue);
+	// reinicia RTT para gerar um novo IRQ
+	RTT_init(pllPreScale, irqRTTvalue);
 
-  // reseta flag
-  flag_rtt = 0;
+	// reseta flag
+	flag_rtt = 0;
 }
 
-/**
- * Função para tocar a sequência
- * seq[]  : Vetor contendo a sequêncua
- * seq_len: Tamanho da sequência
- * delay  : Tempo em ms entre um led e outro
- **/
-int genius_play (int seq[], int seq_len, int delay) {
+volatile char rtt_timeout = 1;
 
+void pisca_led(int n, int t, Pio *p_pio, const uint32_t ul_mask ){
+	for (int i=0;i<n;i++){
+		pio_clear(p_pio, ul_mask);
+		delay_ms(t);
+		pio_set(p_pio, ul_mask);
+		delay_ms(t);
+	}
+}
+/**
+* Função para tocar a sequência
+* seq[]  : Vetor contendo a sequêncua
+* seq_len: Tamanho da sequência
+* delay  : Tempo em ms entre um led e outro
+**/
+
+int genius_play (int seq[], int seq_len, int delay, int seq_mask[]) {
+	for (int i = 0; i<seq_len;i++){
+		if (seq[i] == 1){
+			pisca_led(1,delay,LED1_PIO,LED1_IDX_MASK);
+		}
+		else if(seq[i] == 2){
+			pisca_led(1,delay,LED2_PIO,LED2_IDX_MASK);
+		}
+		else if(seq[i]==3){
+			pisca_led(1,delay,LED3_PIO,LED3_IDX_MASK);
+		}
+	}
 }
 
+
 /**
- * Função que aguardar pelos inputs do usuário
- * e verifica se sequencia está correta ou houve
- * um erro.
- *
- * A função deve retornar:
- *  0: se a sequência foi correta
- *  1: se teve algum erro na sequência (retornar imediatamente)
- */
+* Função que aguardar pelos inputs do usuário
+* e verifica se sequencia está correta ou houve
+* um erro.
+*
+* A função deve retornar:
+*  0: se a sequência foi correta
+*  1: se teve algum erro na sequência (retornar imediatamente)
+*/
+
 int user_play (int seq[], int seq_len) {
-
-}
+		int butAtual = 0;
+		for (int j = 0; j < seq_len; j++){
+			while(!flag_but1 && !flag_but2 && !flag_but3){}
+			
+			if (flag_but1){
+					butAtual = 1;
+					flag_but1 = 0;
+				}else if(flag_but2){
+					butAtual = 2;
+					flag_but2 = 0;
+				}else if(flag_but3){
+					 butAtual = 3;
+					 flag_but3 = 0;
+			}
+			
+			if (butAtual != seq[j]){
+				return 1;
+			}
+		}
+		return 0;
+	}
 
 /**
- * Função que exibe nos LEDs que o jogador acertou
- * Deve manter todos os LEDs acesos por um tempo
- * e então apagar
- */
+* Função que exibe nos LEDs que o jogador acertou
+* Deve manter todos os LEDs acesos por um tempo
+* e então apagar
+*/
 void player_sucess (void) {
-
+	pio_clear(LED1_PIO, LED1_IDX_MASK);
+	pio_clear(LED2_PIO, LED2_IDX_MASK);
+	pio_clear(LED3_PIO, LED3_IDX_MASK);
+	delay_ms(2000);
+	pio_set(LED1_PIO, LED1_IDX_MASK);
+	pio_set(LED2_PIO, LED2_IDX_MASK);
+	pio_set(LED3_PIO, LED3_IDX_MASK);
 }
 
 /**
- * Função que exibe nos LEDs que o jogador errou
- * Deve piscar os LEDs até o usuário apertar um botão
- */
+* Função que exibe nos LEDs que o jogador errou
+* Deve piscar os LEDs até o usuário apertar um botão
+*/
 void player_error (void) {
-
+	pisca_led(1,100,LED1_PIO,LED1_IDX_MASK);
+	pisca_led(1,100,LED2_PIO,LED2_IDX_MASK);
+	pisca_led(1,100,LED3_PIO,LED3_IDX_MASK);
 }
 
 int main (void) {
- 
-  init();
+	
+	init();
+	flag_rtt = 1;
+	
 
-  /* Insert application code here, after the board has been initialized. */
+	/* Insert application code here, after the board has been initialized. */
 	while(1) {
+		
+// 		if (flag_rtt){
+// 			// reinicia RTT para gerar um novo IRQ
+// 			/* IRQ */
+// 			uint16_t pllPreScale = (int) (((float) 32768) / 4.0);
+// 			uint32_t irqRTTvalue = 20;
+// 
+// 			RTT_init(pllPreScale, irqRTTvalue);
+// 			flag_rtt = 0;
+// 			//rtt_pause = !rtt_pause;
+// 			//1= 0
+// 		}
+		
+		delay_ms(500);
+		genius_play(seq0,seq0_len, 1000, seq0_mask);
+		//reset_flags();
+		if(!user_play(seq0, seq0_len)){
+			player_sucess();
+
+			}else{
+			player_error();
+		}
 	}
 }
